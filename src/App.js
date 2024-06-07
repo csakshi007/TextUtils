90462e5d1fead904e4f5b8c54487517a0645e06b
Sakshi <sakshichaudhari777@gmail.com>
2024-06-07 20:06:33 +0530

    Configure React app for deployment to GitHub Pages text utils

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
       <div className="container my-3">
         <TextForm showAlert={showAlert} heading="Enter your text to analyze below:" mode={mode} />
       </div>
-      <About/>
+      {/* <About/> */}
     </>
   );
 }
